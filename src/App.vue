15d9555652d4683b5be45eea0f52ec0b2f3f65d3
Nicolò Luigi Allegris <77175052+MrTip0@users.noreply.github.com>
2021-08-30 20:37:22 +0200

    first release

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="board">
+    <card v-for="(prop, index) in list" :key="index" :prop="prop.value" :show="prop.show" @click="show(index)"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
 
 export default {
+  data() {
+    return({
+      list: [],
+      actual: null,
+      opened: []
+    })
+  },
+  created() {
+    let select = ['❤️', '💵', '🔮', '👻', '😀', '🧠', '❤️', '💵', '🔮', '👻', '😀', '🧠']
+    for (let i = 0; i < 12; i++) {
+      let value = Math.floor(Math.random() * select.length);
+      this.list.push({
+        value: select[value],
+        show: false
+      })
+      let temp = select[value]
+      select[value] = select[select.length - 1]
+      select[select.length - 1] = temp
+      select.pop()
+    }
+  },
+  methods:{
+    show(index){
+      if (!this.opened.includes(index) && this.actual != index && this.list[index].show == false) {
+        this.list[index].show = true
+        if (this.actual == null) {
+          this.actual = index
+        } else {
+          if (this.list[index].value == this.list[this.actual].value) {
+            this.opened.push(index)
+            this.opened.push(this.actual)
+          } else {
+            let def = this.actual
+            setTimeout(()=>{
+              this.list[index].show = false
+              this.list[def].show = false
+            }, 500)
+          }
+          this.actual = null
+        }
+      }
+    }
+  },
   name: 'App',
   components: {
-    HelloWorld
+    Card
   }
 }
 </script>
@@ -22,5 +66,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.board {
+  display: grid;
+  grid-template-columns: 25vw 25vw 25vw;
+  grid-template-rows: auto;
+  gap: 2vw 2vw;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="first">
+    <div class="second" v-if="show">
+      <h1>{{prop}}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    prop: String,
+    show: Boolean
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .first {
+    display: flex;
+    height: 25vw;
+    width: 25vw;
+    border: 1px solid gray;
+    border-radius: 4vw;
+    flex-direction: row;
+    align-items: center;
+  }
+  .second {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  h1 {
+    font-size: 15vw;
+    text-align: center;
+  }
+</style>
